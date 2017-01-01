b6784d0e7c8de523102610f6a3aafdd7d011193a
shenevol <cat3067@gmail.com>
2017-01-02 00:33:33 +0800

    Complete localfileupload, cache keyword input

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine","ejs");
+
+app.get("/", function(req, res){
+	res.render("index");
+});
+
+app.get("/demo", function(req, res){
+	res.render("demo");
+})
+
+app.post("/", function(req, res){
+	console.log(req.uploadFile);
+});
+
+app.listen(3000, function(){
+	console.log("file_demo starts!");
+});
